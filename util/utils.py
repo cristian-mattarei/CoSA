2f89e4febbef68fd4413cb57f51c1d582fe7b52c
Cristian Mattarei <cristian.mattarei@gmail.com>
2018-02-20 10:25:54 -0800

    Added missing file

---
+++ util/utils.py
@@ -0,0 +1,16 @@
+# Copyright 2018 Cristian Mattarei
+#
+# Licensed under the modified BSD (3-clause BSD) License.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+def is_number(strnum):
+    try:
+        int(strnum)
+        return True
+    except:
+        return False
