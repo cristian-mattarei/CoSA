ec668b713379a01d40d82780afba5b8d4ff9c1f7
Cristian Mattarei <mattarei@stanford.edu>
2018-02-27 14:13:29 -0800

    Minor

--- core/printers.py
+++ core/printers.py
@@ -136,6 +136,7 @@ class SMVHTSPrinter(HTSPrinter):
             if formula not in [TRUE(), FALSE()]:
                 self.write("\n%s\n"%keyword)
                 self.printer(formula)
+                self.write("\n")
 
 
         self.write("\n%s\n"%("-"*lenstr))
