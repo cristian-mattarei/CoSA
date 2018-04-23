14c30a39462d7ca7bc898c3c6bd8e751c79fdc5d
makaimann <makaim@stanford.edu>
2018-04-23 13:13:49 -0700

    Handle reg constant replacements

--- cosa/conditions/fold_constants_conditions.py
+++ cosa/conditions/fold_constants_conditions.py
@@ -4,6 +4,7 @@ import coreir
 import argparse
 
 CR = "_const_replacement"
+RCR = "_reg_const_replacement"
 
 def produce_fold_constants_conditions(libs, strfile):
     context = coreir.Context()
@@ -15,28 +16,34 @@ def produce_fold_constants_conditions(libs, strfile):
 
     folded_consts = []
 
+    def _get_info(inst):
+        # Create dictionary out of CoreIR Record
+        assert "value" in inst.config
+        val = inst.config["value"].value
+
+        if type(val) == bool:
+            strval = "1_1" if val else "0_1"
+        else:
+            if type(val) != int:
+                val = val.val
+
+            assert "width" in inst.config
+            width = inst.config["width"].value
+            strval = "{}_{}".format(val, width)
+
+        return strval
+
     # assuming input is flattened
     for inst in td.instances:
         name = inst.selectpath[0]
         if "const" not in inst.module.name:
             continue
+        elif len(name) > len(RCR) and name[-len(RCR):] == RCR:
+            strval = _get_info(inst)
+            folded_consts.append("({}.out = {})".format(name.replace(RCR,""), strval))
         elif len(name) > len(CR) and name[-len(CR):] == CR:
-            # Create dictionary out of CoreIR Record
-            assert "value" in inst.config
-            val = inst.config["value"].value
-
-            if type(val) == bool:
-                strval = "1_1" if val else "0_1"
-            else:
-                if type(val) != int:
-                    val = val.val
-
-                assert "width" in inst.config
-                width = inst.config["width"].value
-                strval = "{}_{}".format(val, width)
-
-
-            folded_consts.append("({} = {})".format(name, strval))
+            strval = _get_info(inst)
+            folded_consts.append("({}.out = {})".format(name.replace(CR,""), strval))
 
     return folded_consts
 
