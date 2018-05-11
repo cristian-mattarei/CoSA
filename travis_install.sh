c807e6defdb2ebf4053f1ee682eb09ee4a23eac5
Cristian Mattarei <cristian.mattarei@gmail.com>
2018-05-10 19:45:12 -0700

    Updated travis file

--- travis_install.sh
+++ travis_install.sh
@@ -9,13 +9,14 @@ if [ ! -f "$PYCOREIR" ]; then
     mv pysmt-15f039f8a2c84b5d8aea10b35d83d3c370b142b6 pysmt
     ls -lah
     cd pysmt
-    pip3 install -e .
+    # pip install -e .
     pysmt-install --msat --confirm-agreement
     pysmt-install --check
     pysmt-install --env
     echo ${TRAVIS_BUILD_DIR}
     cd ..
 
+    export COREIRCONFIG="g++-4.9"
     wget https://github.com/rdaly525/coreir/archive/a20cb469a10f504ebed6ea8a1872bb5baac406c2.zip
     unzip a20cb469a10f504ebed6ea8a1872bb5baac406c2.zip
     rm a20cb469a10f504ebed6ea8a1872bb5baac406c2.zip
@@ -32,7 +33,7 @@ if [ ! -f "$PYCOREIR" ]; then
     sed -i -e 's/KeyError(f"key={key} not in params={self.params.keys()}")/KeyError("key={key} not in params={params_keys}".format(key=key, params_keys=self.params.keys()))/g' coreir/generator.py
     sed -i -e 's/ValueError(f"Arg(name={key}, value={value}) does not match expected type {self.params\[key\].kind}")/ValueError("Arg(name={key}, value={value}) does not match expected type {params_kind}".format(key=key, value=value, params_kind=self.params\[key\].kind))/g' coreir/generator.py
     sed -i -e 's/f"{self.module.name}.{self.name}"/"{module_name}.{self_name}".format(module_name=self.module.name, name=self.name)/g' coreir/wireable.py
-    pip3 install -e .
+    #pip install -e .
 
     popd
 
