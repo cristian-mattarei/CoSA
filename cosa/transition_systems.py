2876f7ab2408d39829c4933c1af70d5789bccc34
Cristian Mattarei <cristian.mattarei@gmail.com>
2018-05-28 12:01:17 -0700

    Minor

--- cosa/transition_systems.py
+++ cosa/transition_systems.py
@@ -179,8 +179,9 @@ class HTS(object):
                     ret[stype] = 0
                 ret[stype] += 1
 
+            for stype in ret:
                 if stype.is_bv_type():
-                    totbits += stype.width
+                    totbits += (stype.width*ret[stype])
                 
             rlist = [(ret[t], str(t)) for t in ret]
             rlist.sort()
