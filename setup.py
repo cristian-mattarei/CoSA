8684e8386d075eaf534ac829096f3e0f3430e4aa
Cristian Mattarei <mattarei@stanford.edu>
2018-08-17 14:41:22 -0700

    Added project description

--- setup.py
+++ setup.py
@@ -1,8 +1,37 @@
 from setuptools import setup, find_packages
 
+long_description=\
+"""================================
+ CoSA: CoreIR Symbolic Analyzer
+================================
+
+CoSA is an SMT-based symbolic model checker for hardware design.
+
+Supported Input Formats
+=======================
+* CoreIR (https://github.com/rdaly525/coreir)
+* BTOR2 (https://github.com/Boolector/btor2tools)
+* Symbolic Transition System
+* Explicit Transition System
+
+Supported Verfication
+======================
+* Invariant Properties
+* LTL Properties
+* Proving capabilities
+* Equivalence Checking
+* Automated Lemma Extraction
+
+CoSA relies on PySMT (http://www.pysmt.org), which is a solver
+agnostic library to interface with SMT solvers.
+
+For more information visit http://github.com/cristian-mattarei/CoSA
+"""
+
 setup(name='CoSA',
-      version='0.2',
+      version='0.21',
       description='CoreIR Symbolic Analyzer',
+      long_description=long_description,
       url='http://github.com/cristian-mattarei/CoSA',
       author='Cristian Mattarei',
       author_email='cristian.mattarei@gmail.com',
