e0b101787e6b7e866c104abdb0aa674cd3dbc1ba
Cristian Mattarei <mattarei@stanford.edu>
2018-10-09 11:48:49 -0700

    Added sorting network

--- cosa/utils/formula_mngm.py
+++ cosa/utils/formula_mngm.py
@@ -11,7 +11,7 @@
 import re
 
 from pysmt.walkers.identitydag import IdentityDagWalker
-from pysmt.shortcuts import Ite, EqualsOrIff, BV, get_type
+from pysmt.shortcuts import Ite, EqualsOrIff, BV, get_type, simplify, And, Or
 from pysmt.typing import BOOL, BVType, ArrayType
 
 from cosa.utils.generic import new_string
@@ -101,3 +101,156 @@ def mem_access(address, locations, width_idx, idx=0):
         return locations[0]
     location = BV(idx, width_idx)
     return Ite(EqualsOrIff(address, location), locations[0], mem_access(address, locations[1:], width_idx, idx+1))
+
+
+class SortingNetwork(object):
+    simplify = False
+    
+    def __init__(self):
+        pass
+
+    def sorting_network(self, inputs, simplify=False):
+        self.simplify = simplify
+        return self.sorting_network_int(inputs)
+    
+    def sorting_network_int(self, inputs):
+        if len(inputs) == 1:
+            return inputs;
+
+        if len(inputs) == 2:
+            el1 = inputs[0]
+            el2 = inputs[1]
+            return self.two_comparator(el1, el2)
+
+        pivot = int(len(inputs) / 2)
+        left_inputs = inputs[:pivot]
+        right_inputs = inputs[pivot:]
+
+        left_outputs = self.sorting_network_int(left_inputs)
+        right_outputs = self.sorting_network_int(right_inputs)
+        
+        outputs = self.merge(left_outputs, right_outputs)
+
+        return outputs
+
+    # Basic comparator for two inputs.
+    def two_comparator(self, input1, input2):
+        if self.simplify:
+            return [simplify(Or(input1, input2)), simplify(And(input1, input2))]
+        else:
+            return [Or(input1, input2), And(input1, input2)]
+
+    # Function that merges two arrays of signals.
+
+    def merge(self, input1, input2):
+        output = []
+
+        if len(input1) == 0:
+            return input2
+
+        if len(input2) == 0:
+            return input1
+
+        if (len(input1) == 1) and (len(input2) == 1):
+            el1 = input1[0]
+            el2 = input2[0];
+            return self.two_comparator(el1, el2);
+
+
+        is_input1_even = ((len(input1) % 2) == 0)
+        is_input2_even = ((len(input2) % 2) == 0)
+
+        is_input1_odd = not is_input1_even
+        is_input2_odd = not is_input2_even
+
+        if is_input1_odd and is_input2_even:
+            return self.merge(input2, input1)
+
+        size_h1 = (len(input1) / 2);
+        res_h1 = (len(input1) % 2);
+        input1_odd = []
+        input1_even = []
+
+        for i in range(len(input1)):
+            element = input1[i]
+            if (((i+1)%2) == 0):
+                input1_even.append(element)
+            else:
+                input1_odd.append(element)
+
+        size_h2 = (len(input2) / 2);
+        res_h2 = (len(input2) % 2);
+        input2_odd = []
+        input2_even = []
+
+        for i in range(len(input2)):
+            element = input2[i]
+            if (((i+1)%2) == 0):
+                input2_even.append(element)
+            else:
+                input2_odd.append(element)
+
+        output_odd = self.merge(input1_odd, input2_odd);
+        output_even = self.merge(input1_even, input2_even);
+
+        # is_input1_even && is_input2_even
+
+        if is_input1_even and is_input2_even:
+            first_output_odd = output_odd[0]
+            last_output_even = output_even[-1]
+            
+            output.append(first_output_odd)
+            
+            for i in range(len(output_odd) - 1):
+                el_odd = output_odd[i+1]
+                el_even = output_even[i]
+
+                res = self.two_comparator(el_odd, el_even)
+                output.append(res[0])
+                output.append(res[1])
+
+            output.append(last_output_even)
+
+        # end of is_input1_even && is_input2_even
+
+        
+        if is_input1_even and is_input2_odd:
+
+            first_output_odd = output_odd[0]
+
+            output.append(first_output_odd)
+
+            for i in range(len(output_even)):
+                el_odd = output_odd[i+1]
+                el_even = output_even[i]
+
+                res = self.two_comparator(el_odd, el_even)
+                output.append(res[0])
+                output.append(res[1])
+
+        # end of is_input1_even && is_input2_odd
+
+
+        if is_input1_odd and is_input2_odd:
+
+            first_output_odd = output_odd[0]
+            last_output_odd = output_odd[-1]
+
+            output.append(first_output_odd)
+
+            for i in range(len(output_even)):
+                el_odd = output_odd[i+1]
+                el_even = output_even[i]
+
+                res = self.two_comparator(el_odd, el_even)
+                output.append(res[0])
+                output.append(res[1])
+
+            output.append(last_output_odd)
+
+        # end of is_input1_odd && is_input2_odd
+
+        assert((len(input1)+len(input2)) == len(output))
+
+        return output
+
