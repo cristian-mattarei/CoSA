90d34a00a4533c3b5c82114bc6507c7a46ebf624
Makai Mann <makaim@stanford.edu>
2019-04-25 14:29:19 -0700

    open as byte file for md5 hashing

--- cosa/analyzers/dispatcher.py
+++ cosa/analyzers/dispatcher.py
@@ -195,7 +195,7 @@ class ProblemSolver(object):
     def md5(self, fname:Path):
         import hashlib
         hash_md5 = hashlib.md5()
-        with fname.open() as f:
+        with fname.open('rb') as f:
             for chunk in iter(lambda: f.read(4096), b""):
                 hash_md5.update(chunk)
         return hash_md5.hexdigest()
