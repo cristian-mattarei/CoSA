ae127cf82e28ed88c22b927f6d65f7e9acf08646
Makai Mann <makaim@stanford.edu>
2019-05-23 14:43:41 -0700

    wget a compressed version of yosys

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -42,4 +42,5 @@ else
 fi
 
 # Get yosys -- using for verilog frontend
-wget http://web.stanford.edu/~makaim/files/yosys
+wget http://web.stanford.edu/~makaim/files/yosys.tar.xz
+tar -xf yosys.tar.xz
