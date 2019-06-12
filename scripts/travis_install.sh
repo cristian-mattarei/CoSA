357acd02da11cb280637778aff856f74c22bdb47
Makai Mann <makaim@stanford.edu>
2019-06-12 12:32:50 -0700

    Revert "Rely on pip for pycoreir installation"
    This reverts commit 5c5d77376fb7ff8ee7852f7c3d40653041799c07.

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+PYCOREIR="`pwd`/pycoreir/setup.py"
+COREIR="`pwd`/coreir/build"
+
+if [ ! -f "$COREIR" ]; then
+    rm -fr coreir*
+    wget https://github.com/rdaly525/coreir/archive/master.zip
+    unzip master.zip
+    rm master.zip
+    mv coreir-master coreir
+    cd coreir
+    mkdir build
+    cd build
+    cmake ..
+    make -j4 && sudo make install
+    cd ../../
+else
+    echo "Skipping COREIR installation"
+    cd coreir/build
+    make -j4 && sudo make install
+    cd ../../
+fi
+
 # Get yosys -- using for verilog frontend
 wget http://web.stanford.edu/~makaim/files/yosys.tar.xz
 tar -xf yosys.tar.xz
