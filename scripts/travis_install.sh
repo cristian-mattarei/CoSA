068d28e7dcc57180b683148a2b7924127cb273db
Makai Mann <makaim@stanford.edu>
2019-11-14 10:43:35 -0800

    Install coreir from source

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # PYCOREIR="`pwd`/pycoreir/setup.py"
-# COREIR="`pwd`/coreir/build"
+COREIR="`pwd`/coreir/build"
 
-# if [ ! -f "$COREIR" ]; then
-#     rm -fr coreir*
-#     wget https://github.com/rdaly525/coreir/archive/master.zip
-#     unzip master.zip
-#     rm master.zip
-#     mv coreir-master coreir
-#     cd coreir
-#     mkdir build
-#     cd build
-#     cmake ..
-#     make -j4 && sudo make install
-#     cd ../../
-# else
-#     echo "Skipping COREIR installation"
-#     cd coreir/build
-#     make -j4 && sudo make install
-#     cd ../../
-# fi
+if [ ! -f "$COREIR" ]; then
+    rm -fr coreir*
+    wget https://github.com/rdaly525/coreir/archive/master.zip
+    unzip master.zip
+    rm master.zip
+    mv coreir-master coreir
+    cd coreir
+    mkdir build
+    cd build
+    cmake ..
+    make -j4 && sudo make install
+    cd ../../
+else
+    echo "Skipping COREIR installation"
+    cd coreir/build
+    make -j4 && sudo make install
+    cd ../../
+fi
 
 # # use latest master instead of release
 # git clone https://github.com/leonardt/pycoreir.git
