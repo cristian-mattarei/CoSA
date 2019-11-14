4cc895f101fe558256b7015bc3c3d882e951ecb2
Makai Mann <makaim@stanford.edu>
2019-11-14 11:01:19 -0800

    Install coreir from master

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 # PYCOREIR="`pwd`/pycoreir/setup.py"
-# COREIR="`pwd`/coreir/build"
+COREIR="`pwd`/coreir/build"
 
-# if [ ! -f "$COREIR" ]; then
-#     rm -fr coreir*
-#     wget https://github.com/rdaly525/coreir/archive/master.zip
-#     unzip master.zip
-#     rm master.zip
-#     mv coreir-master coreir
-#     cd coreir
-#     mkdir build
-#     cd build
-#     cmake ..
-#     make -j4 && sudo make install
-#     cd ../../
-# else
-#     echo "Skipping COREIR installation"
-#     cd coreir/build
-#     make -j4 && sudo make install
-#     cd ../../
-# fi
+if [ ! -f "$COREIR" ]; then
+    rm -fr coreir*
+    wget https://github.com/rdaly525/coreir/archive/master.zip
+    unzip master.zip
+    rm master.zip
+    mv coreir-master coreir
+    cd coreir
+    mkdir build
+    cd build
+    cmake ..
+    make -j4 && sudo make install
+    cd ../../
+else
+    echo "Skipping COREIR installation"
+    cd coreir/build
+    make -j4 && sudo make install
+    cd ../../
+fi
 
-# use latest master instead of release
-git clone https://github.com/leonardt/pycoreir.git
-cd pycoreir
-pip install cmake twine wheel pytest
-pip install -e .
-cd ../
+# # use latest master instead of release
+# git clone https://github.com/leonardt/pycoreir.git
+# cd pycoreir
+# pip install cmake twine wheel pytest
+# pip install -e .
+# cd ../
 
 # Get yosys -- using for verilog frontend
 wget http://web.stanford.edu/~makaim/files/yosys.tar.xz
